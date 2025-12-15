809a45fb69ed69ee8f30f3899bed97dde81127f3
Lorenc Dedaj <le.dedaj@gmail.com>
2025-12-14 19:13:35 -0500

    Add gcp_adapter file to generate yaml and deploy to gcp

--- server/tools/index.js
+++ server/tools/index.js
@@ -1,8 +1,11 @@
 // Central registry for all MCP tools
-import { repo_reader } from "./repo_reader.js";
-import { pipeline_generator } from "./pipeline_generator.js";
-import { oidc_adapter } from "./oidc_adapter.js";
-import { github_adapter } from "./github_adapter.js";
+import { repo_reader } from './repo_reader.js';
+import { pipeline_generator } from './pipeline_generator.js';
+import { oidc_adapter } from './oidc_adapter.js';
+import { github_adapter } from './github_adapter.js';
+
+// Importing the new GCP tool
+import { gcp_adapter } from './gcp_adapter.js';
 
 export const MCP_TOOLS = {
   repo: repo_reader,
@@ -12,6 +15,9 @@ export const MCP_TOOLS = {
   oidc_adapter: oidc_adapter,
   github: github_adapter,
   github_adapter: github_adapter,
+  // Adding the GCP adapter to the MCP tools to be able to call it
+  gcp: gcp_adapter,
+  gcp_adapter: gcp_adapter,
 };
 
 // Optional helper for dynamic access
